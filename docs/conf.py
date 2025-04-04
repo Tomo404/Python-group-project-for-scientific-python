08467acafcf3544612779b3d7a6438fc1e8ff66e
tolta4 <tolvak.tomi@gmail.com>
2025-04-04 08:50:39 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-# Add project root directory to sys.path so Sphinx can find the pandemic module
+sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
