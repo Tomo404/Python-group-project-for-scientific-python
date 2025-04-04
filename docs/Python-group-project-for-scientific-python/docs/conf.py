bb491a83c3680d949d00453acbed5bd01b923034
tolta4 <tolvak.tomi@gmail.com>
2025-04-04 19:17:39 +0200

    Update conf.py

--- docs/Python-group-project-for-scientific-python/docs/conf.py
+++ docs/Python-group-project-for-scientific-python/docs/conf.py
@@ -6,6 +6,17 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+import sys
+from unittest.mock import MagicMock
+
+MOCK_MODULES = ["tkinter", "_tkinter"]
+sys.modules.update((mod, MagicMock()) for mod in MOCK_MODULES)
+os.environ["SPHINX_BUILD"] = "1"
+sys.path.insert(0, os.path.abspath('../')) 
+# sys.path.insert(0, os.path.abspath('../pandemic'))  # Adjust if needed
+
 project = 'pandemic'
 copyright = '2025, Bartha Lilla, Kerényi Kornél, Tolvaj Tamás'
 author = 'Bartha Lilla, Kerényi Kornél, Tolvaj Tamás'
@@ -14,7 +25,9 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
