c35820ec75d323d641e530cbcf5027ddccb8f785
kerko1 <kerenyikornel@gmail.com>
2025-04-12 19:35:15 +0200

    remove faulty check_button_click

--- pandemic/turn_handler.py
+++ pandemic/turn_handler.py
@@ -10,31 +10,6 @@ if not BUILDING_DOCS:
 players = data_unloader.in_game_roles
 current_player_index = 0
 
-def check_button_click():
-    match True:
-        case world_map_drawer.handle_click("drive_ferry"):
-            functions.drive_ferry()
-        case world_map_drawer.handle_click("direct_flight"):
-            functions.direct_flight(current_player_index)
-        case world_map_drawer.handle_click("charter_flight"):
-            functions.charter_flight(current_player_index)
-        case world_map_drawer.handle_click("shuttle_flight"):
-            functions.shuttle_flight()
-        case world_map_drawer.handle_click("build_research_center"):
-            functions.build_research_center(current_player_index)
-        case world_map_drawer.handle_click("treat_disease"):
-            functions.treat_disease()
-        case world_map_drawer.handle_click("share_knowledge"):
-            functions.share_knowledge()
-        case world_map_drawer.handle_click("discover_cure"):
-            functions.discover_cure()
-        case world_map_drawer.handle_click("play_event_card"):
-            functions.play_event_card()
-        case world_map_drawer.handle_click("skip_turn"):
-            functions.skip_turn()
-        case _:
-            print("Unknown button clicked!")
-
 def next_turn():
     global current_player_index
     if functions.check_game_over():
